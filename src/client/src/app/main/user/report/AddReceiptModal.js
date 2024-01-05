2ad7ccf22e5cb04f18bf10e7c5a1ca20a43e30c6
devsolution0112 <developermax902@outlook.com>
2024-01-05 17:25:56 +0100

    - ocr only work when personal receipt

--- src/client/src/app/main/user/report/AddReceiptModal.js
+++ src/client/src/app/main/user/report/AddReceiptModal.js
@@ -212,30 +212,34 @@ export default function AddReceiptModal({
       vat_amount,
     } = data?.data ?? {};
     const { pdf, image } = data.originFile;
-    // if (issued_at) {
-    //   setValue("issued_at", issued_at, {
-    //     shouldDirty: true,
-    //     shouldValidate: true,
-    //   });
-    // }
-    // setValue("total_amount", total_amount ?? "", {
-    //   shouldDirty: true,
-    //   shouldValidate: true,
-    // });
+
+    if (!expense) {
+      if (issued_at) {
+        setValue("issued_at", issued_at, {
+          shouldDirty: true,
+          shouldValidate: true,
+        });
+      }
+      setValue("total_amount", total_amount ?? "", {
+        shouldDirty: true,
+        shouldValidate: true,
+      });
+      setValue("currency", currencyCode, {
+        shouldDirty: true,
+        shouldValidate: true,
+      });
+      if (currencyCode == "EUR") {
+        setValue("amount_eur", total_amount ?? "", {
+          shouldDirty: true,
+          shouldValidate: true,
+        });
+      }
+    }
+
     setValue("vat_amount", vat_amount ?? "", {
       shouldDirty: true,
       shouldValidate: true,
     });
-    // setValue("currency", currencyCode, {
-    //   shouldDirty: true,
-    //   shouldValidate: true,
-    // });
-    // if (currencyCode == "EUR") {
-    //   setValue("amount_eur", total_amount ?? "", {
-    //     shouldDirty: true,
-    //     shouldValidate: true,
-    //   });
-    // }
 
     if (image[0] === ".") setReceiptImage(image.slice(2));
     else setReceiptImage(image);
